696764804734ae2bd0f51bfb99668b413f005c9d
Andrew Hunt <andrew@hunt.li>
2013-03-12 11:06:12 -0400

    initial vim dir commit

---
+++ vimInstaller.sh
@@ -0,0 +1,6 @@
+mkdir -p ~/.vim/autoload ~/.vim/bundle; \
+curl -Sso ~/.vim/autoload/pathogen.vim \
+https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+
+git submodule init
+git submodule update
